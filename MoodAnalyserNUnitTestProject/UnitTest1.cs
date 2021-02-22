f610d98d63d3173d643e858bb8aab14be4f1673d
diliprathod32 <diliprathod32@gmail.com>
2021-02-22 20:48:14 +0530

    [Dilip] Welcome to Mood Analyser Exception Handling and Testing

---
+++ MoodAnalyserNUnitTestProject/UnitTest1.cs
@@ -0,0 +1,61 @@
+using NUnit.Framework;
+using MoodAnalyserRepeat;
+namespace MoodAnalyserNUnitTestProject
+{
+    public class Tests
+    {
+        MoodAnalyser moodAnalyser;
+        [SetUp]
+        public void Setup()
+        {
+            moodAnalyser = new MoodAnalyser();
+        }
+        /// <summary>
+        /// TC1.1 Sad mood message should return sad message
+        /// </summary>
+        [Test]
+        public void SadMoodShouldReturnSad()
+        {
+            string sadMessage = "I am in sad Mood";
+            string expected="SAD";
+            string actual = moodAnalyser.MoodAnalyserMethod(sadMessage);
+            Assert.AreEqual(expected,actual);
+        }
+        /// <summary>
+        /// TC1.2 I am any mood should return happy message
+        /// </summary>
+        [Test]
+        public void AnyMoodShouldReturnHappy()
+        {
+            string anymoodMessage = "I am in any mood";
+            string expected = "HAPPY";
+            string actual = moodAnalyser.MoodAnalyserMethod(anymoodMessage);
+            Assert.AreEqual(expected, actual);
+        }
+        /// <summary>
+        /// Refactor TC1.1
+        /// Paramerters constructor
+        /// </summary>
+        [Test]
+        public void ConstructorInSadMoodShouldReturnSad()
+        {
+            moodAnalyser = new MoodAnalyser("I am in sad mood ");//parameter constructor
+            string expected = "SAD";
+            string actual = moodAnalyser.MoodAnalyseConstructorMethod();
+            Assert.AreEqual(expected, actual);
+        }
+
+        /// <summary>
+        /// Refactor TC1.2
+        /// Paramerters constructor
+        /// </summary>
+        [Test]
+        public void ConstructorInHappyMoodShouldReturnHappy()
+        {
+            moodAnalyser = new MoodAnalyser("I am in Happy mood ");//parameter constructor
+            string expected = "HAPPY";
+            string actual = moodAnalyser.MoodAnalyseConstructorMethod();
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ MoodAnalyserRepeat/MoodAnalyser.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MoodAnalyserRepeat
+{
+    public class MoodAnalyser
+    {
+        string message;
+        public MoodAnalyser()
+        {
+
+        }
+        /// <summary>
+        /// parameterized constructor
+        /// </summary>
+        /// <param name="message"></param>
+        public MoodAnalyser(string message)
+        {
+            this.message = message;
+        }
+        /// <summary>
+        /// ability to analyse and respond happy or sad mood
+        /// </summary>
+        /// <param name="message"></param>
+        /// <returns></returns>
+        public string MoodAnalyserMethod(string message)
+        {
+            if (message.ToLower().Contains("sad"))
+               return "SAD";
+            else
+                return "HAPPY";
+        }
+        /// <summary>
+        /// Constructor method 
+        /// </summary>
+        /// <returns></returns>
+        public string MoodAnalyseConstructorMethod()
+        {
+            if (this.message.ToLower().Contains("sad"))
+            {
+                return "SAD";
+            }
+            else
+            {
+                return "HAPPY";
+            }
+        }
+    }
+}
